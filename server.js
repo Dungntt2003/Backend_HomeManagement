c98b62271cfd0d691aee3e421a8737d9f86b1411
Dungntt2003 <nguyenthuydung55555@gmail.com>
2024-03-21 22:39:27 +0700

    Test store image in database

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 // test upload image with multer
 const multer = require("multer");
 const path = require("path");
 const app = express();
 const port = 8000;
+const pool = require("./db");
 
 const HomeRoute = require("./api/routes/home");
 
@@ -19,9 +21,11 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
+app.use("/images", express.static(__dirname + "/images"));
 app.use(express.json());
 app.use(morgan("combined"));
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -37,8 +41,22 @@ app.use((req, res, next) => {
 });
 
 app.use("/homes", HomeRoute);
-app.post("/upload", upload.array("images", 12), (req, res, next) => {
-  res.send("Upload successful");
+app.post("/upload", upload.single("image"), (req, res, next) => {
+  const id = req.body.id;
+  const file = req.file.filename;
+  const sql = 'INSERT INTO Persons ("personid", "image") VALUES ($1, $2)';
+  pool.query(sql, [id, file], (error, result) => {
+    if (error) return res.json({ message: error.message });
+    return res.json({ message: "Successfully created" });
+  });
+});
+
+app.get("/upload", (req, res, next) => {
+  const sql = "SELECT * FROM Persons";
+  pool.query(sql, (error, result) => {
+    if (error) return res.json({ message: error.message });
+    return res.json(result.rows);
+  });
 });
 
 app.listen(port, () => {
