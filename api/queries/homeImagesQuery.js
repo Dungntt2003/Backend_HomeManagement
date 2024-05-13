1b03e964a8c9fc0853a8cf136a21d2cc9d215864
Dungntt2003 <nguyenthuydung55555@gmail.com>
2024-05-13 12:39:51 +0700

    Update room api with update

--- api/queries/homeImagesQuery.js
+++ api/queries/homeImagesQuery.js
@@ -1,7 +1,10 @@
 const getAllImages = "SELECT * FROM roomImages";
 const getARoomImages = "SELECT images FROM roomImages where room_id = $1";
+const postImage =
+  "UPDATE roomImages SET images = images || $1 where room_id = $2";
 
 module.exports = {
   getAllImages,
   getARoomImages,
+  postImage,
 };
--- server.js
+++ server.js
@@ -51,12 +51,28 @@ app.use("/", LoginRoute);
 app.post("/upload", upload.array("images", 5), (req, res, next) => {
   const id = req.body.id;
   const files = req.files.map((file) => file.filename);
-  const sql = "INSERT INTO roomImages (room_id, images) VALUES ($1, $2)";
-  pool.query(sql, [id, files], (error, result) => {
-    if (error) return res.json({ message: error.message });
-    return res.json({ message: "Successfully created" });
+  const checkRoom = "SELECT * from roomImages where room_id = $1";
+  const insert = "INSERT INTO roomImages (room_id, images) VALUES ($1,$2)";
+  const sql = "UPDATE roomImages SET images = $1 where room_id = $2";
+  pool.query(checkRoom, [id], (err, result) => {
+    if (err)
+      return result.status(409).json({
+        message: err.message,
+      });
+    if (result.rows.length > 0) {
+      pool.query(sql, [files, id], (error, result) => {
+        if (error) return res.json({ message: error.message });
+        return res.json({ message: "Successfully created" });
+      });
+    } else {
+      pool.query(insert, [id, files], (err, result) => {
+        if (err) return res.status(409).json({ message: err.message });
+        return res.status(200).json({ message: "Successfully created" });
+      });
+    }
   });
 });
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
