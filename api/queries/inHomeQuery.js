23eb4f79854ec5454c1cc0403926cd485bd1c6a4
Dungntt2003 <nguyenthuydung55555@gmail.com>
2024-06-17 01:29:35 +0700

    Update renter api

--- api/queries/inHomeQuery.js
+++ api/queries/inHomeQuery.js
@@ -5,7 +5,7 @@ const updateRenterDate = "UPDATE inRoom SET endDate = $1 where id = $2";
 const stopRenter = "UPDATE inRoom SET endDate = CURRENT_DATE WHERE id = $1";
 const getAll = "SELECT * FROM inRoom";
 const getRenterByRoom =
-  "SELECT * FROM inRoom join users on inRoom.user_id = users.id where room_id = $1";
+  "SELECT * FROM users join inRoom on inRoom.user_id = users.id where room_id = $1 and endDate > CURRENT_DATE";
 const getBill = "SELECT * FROM bill WHERE room_id = $1";
 const updateBill =
   "UPDATE bill SET post_date = CURRENT_DATE, ispay = true WHERE id = $1";
